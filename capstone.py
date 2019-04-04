a8f1452b614a8f9133395b6b28e7ac5be5c24c1a
20danika25 <48740318+20danika25@users.noreply.github.com>
2019-04-04 09:44:23 +0900

    Added ball + paddle movement
    Started work on the ball class, and fixed the paddle boundary. Added keyboard bindings for paddle control.

--- capstone.py
+++ capstone.py
@@ -8,22 +8,75 @@ import spgl
 
 # Create Classes
 class Paddle(spgl.Sprite):
-    def __init__(self, shape, color, x, y):
-        spgl.Sprite.__init__(self, shape, color, x, y)
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.speed = 0
+	
+	def tick(self):
+		self.move()
+		
+	def move(self):
+		self.fd(self.speed)
+		
+		if self.xcor() > game.SCREEN_WIDTH / 2 - 20:
+			self.goto(game.SCREEN_WIDTH / 2 - 20, self.ycor())
+			self.speed = 0
+			self.setheading(0)
+			
+		if self.xcor() < -game.SCREEN_WIDTH / 2 + 10:
+			self.goto(-game.SCREEN_WIDTH / 2 + 10, self.ycor())
+			self.speed = 0
+			self.setheading(180)
+			
+	def move_right(self):
+		self.setheading(0)
+		self.speed = 8
+	
+	
+	def move_left(self):
+		self.setheading(180)
+		self.speed = 8
+		
 
+class Ball(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.speed = 0
+		
+		
+	def move(self):
+		self.fd(self.speed)
+		
+		if self.xcor() > game.SCREEN_WIDTH / 2 - 20:
+			self.goto(game.SCREEN_WIDTH / 2 - 20, self.ycor())
+
+			
+		if self.xcor() < -game.SCREEN_WIDTH / 2 + 10:
+			self.goto(-game.SCREEN_WIDTH / 2 + 10, self.ycor())
+
+		
+		if self.ycor() > game.SCREEN_HEIGHT:
+			self.goto(self.xcor, game.SCREEN_HEIGHT)
+			self.rt(40)
+		
+			
+		
+		
 # Create Functions
 
 # Initial Game setup
-game = spgl.Game(800, 600, "blue", "Dakinoid By Dakina", 0)
+game = spgl.Game(800, 600, "black", "Dakinoid By Dakina", 0)
 
 # Create Sprites
 paddle = Paddle("square", "white", 0, -250)
-
+ball = Ball("circle", "skyblue", 0, -229)
 # Create Labels
 
 # Create Buttons
 
 # Set Keyboard Bindings
+game.set_keyboard_binding(spgl.KEY_LEFT, paddle.move_left)
+game.set_keyboard_binding(spgl.KEY_RIGHT, paddle.move_right)
 
 while True:
     # Call the game tick method
