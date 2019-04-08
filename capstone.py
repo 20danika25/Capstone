6c2b57047b55b32e9bbee7fdc119ba47d8d216dd
20danika25 <48740318+20danika25@users.noreply.github.com>
2019-04-08 15:30:13 +0900

    added paddleball and boundary collision
    cool

--- capstone.py
+++ capstone.py
@@ -5,6 +5,7 @@
 
 # Import SPGL
 import spgl
+import random
 
 # Create Classes
 class Paddle(spgl.Sprite):
@@ -42,25 +43,37 @@ class Ball(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
 		self.speed = 0
+		self.dx = 0
+		self.dy = 0
+
 		
+	def tick(self):
+		self.move()	
 		
 	def move(self):
-		self.fd(self.speed)
+		self.goto(self.xcor() + self.dx, self.ycor() + self.dy)
 		
 		if self.xcor() > game.SCREEN_WIDTH / 2 - 20:
-			self.goto(game.SCREEN_WIDTH / 2 - 20, self.ycor())
-			self.dx * -1
+			self.dx *= -1
+			
 			
 		if self.xcor() < -game.SCREEN_WIDTH / 2 + 10:
-			self.goto(-game.SCREEN_WIDTH / 2 + 10, self.ycor())
-			self.dx * -1
-		
-		if self.ycor() > game.SCREEN_HEIGHT:
-			self.goto(self.xcor, game.SCREEN_HEIGHT)
-			self.dy * -1
-		
+			self.dx *= -1
 			
-		#check for collision with the paddle
+		
+		if self.ycor() > game.SCREEN_HEIGHT / 2 - 10:
+			self.dy *= -1
+			
+		
+	def start_moving(self):
+		self.dx = 5
+		self.dy = 5
+		self.heading = random.randint(50,130)
+			
+		#check for collision with the paddle, if there is a collision,
+		#set the dy of the ball to -1 so it bounces off
+		
+		
 		
 		
 class brick(spgl.Sprite):
@@ -82,7 +95,12 @@ game = spgl.Game(800, 600, "black", "Dakinoid By Dakina", 0)
 
 # Create Sprites
 paddle = Paddle("square", "white", 0, -250)
-ball = Ball("circle", "skyblue", 0, -229)
+paddle.width = 100
+paddle.height = 20
+paddle.shapesize(stretch_wid=1, stretch_len=5, outline=None)
+ball = Ball("triangle", "skyblue", 0, -229)
+ball.width = 20
+ball.height = 20
 # Create Labels
 
 # Create Buttons
@@ -90,7 +108,11 @@ ball = Ball("circle", "skyblue", 0, -229)
 # Set Keyboard Bindings
 game.set_keyboard_binding(spgl.KEY_LEFT, paddle.move_left)
 game.set_keyboard_binding(spgl.KEY_RIGHT, paddle.move_right)
-
+game.set_keyboard_binding(spgl.KEY_SPACE, ball.start_moving)
 while True:
     # Call the game tick method
     game.tick()
+    
+    # check for collision between ball and paddle
+    if game.is_collision(paddle, ball):
+    	ball.dy *= -1
