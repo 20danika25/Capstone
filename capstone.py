675070f70bbab7e0d183b8909963a84ff74de99d
20danika25 <48740318+20danika25@users.noreply.github.com>
2019-04-11 09:54:10 +0900

    Added brick loop
    cool

--- capstone.py
+++ capstone.py
@@ -31,12 +31,12 @@ class Paddle(spgl.Sprite):
 			
 	def move_right(self):
 		self.setheading(0)
-		self.speed = 8
+		self.speed = 10
 	
 	
 	def move_left(self):
 		self.setheading(180)
-		self.speed = 8
+		self.speed = 10
 		
 
 class Ball(spgl.Sprite):
@@ -44,7 +44,6 @@ class Ball(spgl.Sprite):
 		spgl.Sprite.__init__(self, shape, color, x, y)
 		self.speed = 0
 		self.dx = 0
-		self.dy = 0
 
 		
 	def tick(self):
@@ -63,44 +62,101 @@ class Ball(spgl.Sprite):
 		
 		if self.ycor() > game.SCREEN_HEIGHT / 2 - 10:
 			self.dy *= -1
-			
+		
+		if self.ycor() < -game.SCREEN_HEIGHT / 2 + 20:
+			self.goto(0,-229)
+			self.dy = 0
+			self.dx = 0
+
+
 		
 	def start_moving(self):
 		self.dx = 5
 		self.dy = 5
 		self.heading = random.randint(50,130)
 			
-		#check for collision with the paddle, if there is a collision,
-		#set the dy of the ball to -1 so it bounces off
+	
 		
-		
-		
-		
-class brick(spgl.Sprite):
+class Brick(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.speed = 0
+		self.width = 50
+		self.height = 20
+		self.shapesize(stretch_wid=1, stretch_len=2.5, outline=None)
 		
+
 		
-		#check for collision with ball. if there is a collision, 
-		#move the brick sprite off of the screen
-		
-class powerup(spgl.Sprite):
+class Powerup(Brick):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
+		Brick.__init__(self,shape,color,x, y)
 		
+		
+class Pen(spgl.Sprite): # for the actual bricks
+	def __init__(self):
+		spgl.Sprite.__init__(self)
+		self.shape("square")
+		self.color(white)
+		self.penup()
+		self.speed(0)
+		
+# create a list of levels
+levels = [""]
+
+# create a list of bricks
+bricks = []
+
+#define the first level
+level_1 = [
+"                                           ",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"X                                         X",
+"                                           ",
+]
+# append level 1 to levels
+levels.append(level_1)
 # Create Functions
+	# function to draw the level.
+def draw_level(level):
+	for y in range(len(level)):
+		for x in range(len(level[y])):
+			if level[y][x] == "X":
+				screen_x = -300 + (x * 16)
+				screen_y = 300 - (y * 30)
+			
+				brick = Brick("square", "white", screen_x, screen_y)
+				bricks.append(brick)
+
+
 
 # Initial Game setup
 game = spgl.Game(800, 600, "black", "Dakinoid By Dakina", 0)
+
+draw_level(level_1)
 
 # Create Sprites
 paddle = Paddle("square", "white", 0, -250)
 paddle.width = 100
 paddle.height = 20
 paddle.shapesize(stretch_wid=1, stretch_len=5, outline=None)
-ball = Ball("triangle", "skyblue", 0, -229)
+ball = Ball("circle", "skyblue", 0, -229)
 ball.width = 20
 ball.height = 20
+
+
 # Create Labels
 
 # Create Buttons
@@ -110,9 +166,18 @@ game.set_keyboard_binding(spgl.KEY_LEFT, paddle.move_left)
 game.set_keyboard_binding(spgl.KEY_RIGHT, paddle.move_right)
 game.set_keyboard_binding(spgl.KEY_SPACE, ball.start_moving)
 while True:
-    # Call the game tick method
-    game.tick()
+	# Call the game tick method
+	game.tick()
+
+	# check for collision between ball and paddle
+	if game.is_collision(paddle, ball):
+		ball.dy *= -1
+    	
+	# check for collision between ball and brick
+	for brick in bricks:
+		if game.is_collision(brick, ball):
+			print("hit")
+			ball.dy *= -1
+			brick.destroy()
     
-    # check for collision between ball and paddle
-    if game.is_collision(paddle, ball):
-    	ball.dy *= -1
+
